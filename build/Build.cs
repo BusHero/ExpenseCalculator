134609a5652e342bb8047ba4baa808862b97b283
bus1hero <petru.cervac@gmail.com>
2024-01-15 22:06:04 +0200

    build: add support for nuke

--- build/Build.cs
+++ build/Build.cs
@@ -1,29 +1,47 @@
 using Nuke.Common;
+using Nuke.Common.Tools.DotNet;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
 
 class Build : NukeBuild
 {
-    public static int Main () 
-        => Execute<Build>(x => x.Compile);
+    public static int Main () => Execute<Build>(x => x.Compile);
 
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
-    readonly Configuration Configuration 
-        = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+    readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
 
-    Target Clean => x => x
+    Target Clean => _ => _
         .Before(Restore)
         .Executes(() =>
         {
+            DotNetClean(_ => _
+                .EnableNoLogo()
+                .SetConfiguration(Configuration));
         });
 
-    Target Restore => x => x
+    Target Restore => _ => _
         .Executes(() =>
         {
+            DotNetRestore();
         });
 
-    Target Compile => x => x
+    Target Compile => _ => _
         .DependsOn(Restore)
         .Executes(() =>
         {
+            DotNetBuild(_ => _
+                .EnableNoRestore()
+                .EnableNoLogo()
+                .SetConfiguration(Configuration));
         });
 
+    Target Test => _ => _
+        .DependsOn(Compile)
+        .Executes(() =>
+        {
+            DotNetTest(_ => _
+                .EnableNoBuild()
+                .EnableNoRestore()
+                .EnableNoLogo()
+                .SetConfiguration(Configuration));
+        });
 }
