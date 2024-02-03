6ef59b04c02532c198228825f1486dec20aeccd9
bus1hero <petru.cervac@gmail.com>
2024-02-03 23:03:22 +0200

    Add required paramters

--- build/Build.Azure.cs
+++ build/Build.Azure.cs
@@ -7,6 +7,7 @@ using Serilog;
 
 partial class Build
 {
+    [UsedImplicitly]
     Target Compress => _ => _
         .DependsOn(Publish)
         .Executes(() =>
@@ -17,35 +18,38 @@ partial class Build
 
     
     [Parameter("The id of the application to deploy to"), Secret]
-    Guid AzureApplicationId { get; set; }
+    Guid? AzureApplicationId { get; set; }
 
     [Parameter("Tenant Id"), Secret]
-    Guid AzureTenantId { get; set; }
+    Guid? AzureTenantId { get; set; }
 
-    [Parameter, Secret]
-    string AzureDeploySecret { get; set; }
+    [Parameter, Secret] string AzureDeploySecret { get; set; }
     
     [Parameter] string Artifact { get; set; }
 
     [UsedImplicitly]
     Target LoginToAzure => _ => _
+        .Requires(() => AzureDeploySecret)
+        .Requires(() => AzureApplicationId)
+        .Requires(() => AzureTenantId)
         .Executes(async () =>
         {
             await Cli.Wrap("az")
                 .WithArguments(args => args
                     .Add("login")
                     .Add("--service-principal")
-                    .Add("--user").Add(AzureApplicationId)
+                    .Add("--user").Add(AzureApplicationId!)
                     .Add("--password").Add(AzureDeploySecret)
-                    .Add("--tenant").Add(AzureTenantId))
-                .WithStandardOutputPipe(PipeTarget.ToDelegate(x => Log.Information("{MSG}", x)))
-                .WithStandardErrorPipe(PipeTarget.ToDelegate(x => Log.Debug("{MSG}", x)))
+                    .Add("--tenant").Add(AzureTenantId!))
+                .WithStandardOutputPipe(PipeTarget.ToDelegate(x => Log.Information("{Msg}", x)))
+                .WithStandardErrorPipe(PipeTarget.ToDelegate(x => Log.Debug("{Msg}", x)))
                 .ExecuteAsync();
         });
     
     [UsedImplicitly]
     Target DeployToAzure => _ => _
         .DependsOn(LoginToAzure)
+        .Requires(() => Artifact)
         .Executes(async () =>
         {
             await Cli.Wrap("az")
@@ -56,8 +60,8 @@ partial class Build
                     .Add(["--name", "bus1hero"])
                     .Add(["--src-path", Artifact])
                 )
-                .WithStandardOutputPipe(PipeTarget.ToDelegate(x => Log.Information("{MSG}", x)))
-                .WithStandardErrorPipe(PipeTarget.ToDelegate(x => Log.Debug("{MSG}", x)))
+                .WithStandardOutputPipe(PipeTarget.ToDelegate(x => Log.Information("{Msg}", x)))
+                .WithStandardErrorPipe(PipeTarget.ToDelegate(x => Log.Debug("{Msg}", x)))
                 .ExecuteAsync();
         });
 }
