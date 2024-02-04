adbad34d633bdc997367d590305032cecc547f99
Cervac Petru <petru.cervac@gmail.com>
2024-02-04 20:39:44 +0200

    ci: fix download artifact action (#7)
    * ci: fix download artifact action
    
    * ci: remove variables
    
    * build: upload artifact
    
    * Add required paramters
    
    * ci: update stuff
    
    * add debug step
    
    * change job os
    
    * asd
    
    * ci: fix deploying application
    
    * asd
    
    * asd
    
    * build: update required parameters
    
    * ci: cleanup workflow

--- build/Build.Azure.cs
+++ build/Build.Azure.cs
@@ -3,59 +3,69 @@ using System.IO.Compression;
 using CliWrap;
 using JetBrains.Annotations;
 using Nuke.Common;
+using Nuke.Common.IO;
 using Serilog;
 
 partial class Build
 {
-    Target Compress => _ => _
-        .DependsOn(Publish)
-        .Executes(() =>
-        {
-            ZipFile.CreateFromDirectory(RootDirectory / "output" / "publish", RootDirectory / "publish.zip"); 
-            Log.Information("Zip file created {Path}", RootDirectory / "publish.zip");
-        });
-
-    
     [Parameter("The id of the application to deploy to"), Secret]
-    Guid AzureApplicationId { get; set; }
+    Guid? AzureApplicationId { get; set; }
 
     [Parameter("Tenant Id"), Secret]
-    Guid AzureTenantId { get; set; }
+    Guid? AzureTenantId { get; set; }
 
-    [Parameter, Secret]
-    string AzureDeploySecret { get; set; }
+    [Parameter, Secret] string AzureDeploySecret { get; set; }
+    
+    [Parameter, Secret] string ResourceGroup { get; set; }
+    [Parameter, Secret] string AppName { get; set; }
+    
+    [Parameter] string ArtifactsPath { get; set; }
+    
+    AbsolutePath Artifact { get; set; }
+    
+    [UsedImplicitly]
+    Target Compress => _ => _
+        .Requires(() => ArtifactsPath)
+        .Executes(() =>
+        {
+            Artifact = TemporaryDirectory / $"{Guid.NewGuid()}.zip";
+            ZipFile.CreateFromDirectory(RootDirectory / ArtifactsPath, Artifact); 
+            Log.Information("Zip file created {Path}", Artifact);
+        });
 
     [UsedImplicitly]
     Target LoginToAzure => _ => _
+        .Requires(() => AzureDeploySecret)
+        .Requires(() => AzureApplicationId)
+        .Requires(() => AzureTenantId)
         .Executes(async () =>
         {
             await Cli.Wrap("az")
                 .WithArguments(args => args
                     .Add("login")
                     .Add("--service-principal")
-                    .Add("--user").Add(AzureApplicationId)
+                    .Add("--user").Add(AzureApplicationId!)
                     .Add("--password").Add(AzureDeploySecret)
-                    .Add("--tenant").Add(AzureTenantId))
-                .WithStandardOutputPipe(PipeTarget.ToDelegate(x => Log.Information("{MSG}", x)))
-                .WithStandardErrorPipe(PipeTarget.ToDelegate(x => Log.Debug("{MSG}", x)))
+                    .Add("--tenant").Add(AzureTenantId!))
+                .WithStandardOutputPipe(PipeTarget.ToDelegate(x => Log.Information("{Msg}", x)))
+                .WithStandardErrorPipe(PipeTarget.ToDelegate(x => Log.Debug("{Msg}", x)))
                 .ExecuteAsync();
         });
     
     [UsedImplicitly]
     Target DeployToAzure => _ => _
-        .DependsOn(LoginToAzure)
+        .DependsOn(LoginToAzure, Compress)
         .Executes(async () =>
         {
             await Cli.Wrap("az")
                 .WithArguments(args => args
                     .Add("webapp")
                     .Add("deploy")
-                    .Add(["--resource-group", "bus1heroDevEnvSetup"])
-                    .Add(["--name", "bus1hero"])
-                    .Add(["--src-path", RootDirectory / "publish.zip"])
-                )
-                .WithStandardOutputPipe(PipeTarget.ToDelegate(x => Log.Information("{MSG}", x)))
-                .WithStandardErrorPipe(PipeTarget.ToDelegate(x => Log.Debug("{MSG}", x)))
+                    .Add("--resource-group").Add(ResourceGroup)
+                    .Add("--name").Add(AppName)
+                    .Add("--src-path").Add(Artifact.ToString()))
+                .WithStandardOutputPipe(PipeTarget.ToDelegate(x => Log.Information("{Msg}", x)))
+                .WithStandardErrorPipe(PipeTarget.ToDelegate(x => Log.Debug("{Msg}", x)))
                 .ExecuteAsync();
         });
 }
