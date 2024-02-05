6f9c043e6854a4f2373828a313a19c7bec98c5b1
bus1hero <petru.cervac@gmail.com>
2024-02-05 19:04:40 +0200

    chore: remove dead code

--- ExpenseManager/Pages/Create.cshtml.cs
+++ ExpenseManager/Pages/Create.cshtml.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ExpenseManager.Pages;
 
-// [IgnoreAntiforgeryToken(Order = 1001)]
 public class Create : PageModel
 {
     public void OnGet()
