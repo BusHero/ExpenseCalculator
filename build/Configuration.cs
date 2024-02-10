306e152af9ddb49854d281f28ade6019118b1383
bus1hero <petru.cervac@gmail.com>
2024-02-10 22:24:16 +0200

    build: enable treating warnings as errors

--- build/Configuration.cs
+++ build/Configuration.cs
@@ -2,7 +2,7 @@ using System.ComponentModel;
 using Nuke.Common.Tooling;
 
 [TypeConverter(typeof(TypeConverter<Configuration>))]
-public class Configuration : Enumeration
+class Configuration : Enumeration
 {
     public static Configuration Debug = new Configuration { Value = nameof(Debug) };
     public static Configuration Release = new Configuration { Value = nameof(Release) };
