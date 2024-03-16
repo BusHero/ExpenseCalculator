b3832e49b78f9e874264bf8c9bd0781d8d2afd75
bus1hero <petru.cervac@gmail.com>
2024-03-16 14:35:39 +0200

    test: update integration testing configuration

--- ExpenseManager.IntegrationTests/MyWebFactory.cs
+++ ExpenseManager.IntegrationTests/MyWebFactory.cs
@@ -1,8 +1,12 @@
+using System.Data.Common;
 using ExpenseManager.Domain;
+using ExpensesManager.DataAccess;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 
@@ -12,6 +16,34 @@ public class MyWebFactory : WebApplicationFactory<Program>
 {
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
+        builder.ConfigureServices(services =>
+        {
+            var dbContextDescriptor = services.SingleOrDefault(
+                d => d.ServiceType ==
+                     typeof(DbContextOptions<ApplicationContext>));
+
+            services.Remove(dbContextDescriptor!);
+
+            var dbConnectionDescriptor = services.SingleOrDefault(
+                d => d.ServiceType ==
+                     typeof(DbConnection));
+
+            services.Remove(dbConnectionDescriptor!);
+            
+            services.AddSingleton<DbConnection>(container =>
+            {
+                var connection = new SqliteConnection("DataSource=:memory:");
+                connection.Open();
+
+                return connection;
+            });
+
+            services.AddDbContext<ApplicationContext>((container, options) =>
+            {
+                var connection = container.GetRequiredService<DbConnection>();
+                options.UseSqlite(connection);
+            });
+        });
         builder
             .ConfigureTestServices(services =>
             {
@@ -31,6 +63,7 @@ public class MyWebFactory : WebApplicationFactory<Program>
                 });
             });
     }
+    
     public void WithUserAuthenticationStatus(bool authStatus)
     {
         TestAuthHandler.SetAuthStatus(authStatus);
