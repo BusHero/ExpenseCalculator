95d19a2b5e90de1e3cfc50af23a724dae02c908a
bus1hero <petru.cervac@gmail.com>
2024-03-31 23:37:25 +0300

    feat: automatically redirect on logout

--- IdentityServer/IdentityServerAspNetIdentity/Pages/Account/Logout/LogoutOptions.cs
+++ IdentityServer/IdentityServerAspNetIdentity/Pages/Account/Logout/LogoutOptions.cs
@@ -6,6 +6,6 @@ namespace IdentityServerAspNetIdentity.Pages.Logout;
 
 public static class LogoutOptions
 {
-    public static readonly bool ShowLogoutPrompt = true;
-    public static readonly bool AutomaticRedirectAfterSignOut = false;
+    public const bool ShowLogoutPrompt = true;
+    public const bool AutomaticRedirectAfterSignOut = true;
 }
