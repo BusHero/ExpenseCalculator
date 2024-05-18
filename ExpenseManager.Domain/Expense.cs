fd13ccf4d11e2ce1b8c4bfac2298932eb5a67e5d
bus1hero <petru.cervac@gmail.com>
2024-05-18 22:04:32 +0300

    feat: move Create to index page

--- ExpenseManager.Domain/Expense.cs
+++ ExpenseManager.Domain/Expense.cs
@@ -5,4 +5,6 @@ public sealed record Expense
     public required ExpenseName Name { get; init; }
     
     public required Money Amount { get; init; }
+    
+    public required DateTime Date { get; init; }
 }
--- ExpenseManager/Pages/Index.cshtml.cs
+++ ExpenseManager/Pages/Index.cshtml.cs
@@ -1,5 +1,8 @@
+using System.ComponentModel.DataAnnotations;
 using System.Security.Claims;
 using ExpenseManager.Domain;
+using ExpensesManager.DataAccess;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -9,7 +12,43 @@ namespace ExpenseManager.Pages;
 [AllowAnonymous]
 public class IndexModel : PageModel
 {
-    public IndexDto? Data { get; private set; }
+    internal IndexDto? Data { get; private set; }
+
+    [ModelBinder] public Data Data1 { get; set; } = null!;
+
+    public async Task<IActionResult> OnPost(
+        [FromServices] IApplicationService applicationService,
+        [FromServices] ApplicationContext context)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest();
+        }
+
+        var result = await HttpContext.AuthenticateAsync();
+
+        if (!(result.Succeeded && (result.Principal.Identity?.IsAuthenticated ?? false)))
+        {
+            return Forbid();
+        }
+        // if (!result.Succeeded || !(result.Principal.Identity?.IsAuthenticated ?? false))
+        // {
+        //     return Forbid();
+        // }
+
+        var userId = User.FindFirstValue("sub") ?? string.Empty;
+
+        applicationService.AddExpenseToLoggedInUser(
+            ExternalUserId.FromString(userId),
+            new Expense
+            {
+                Name = ExpenseName.FromString(Data1.Expense),
+                Amount = Money.FromDecimal(Data1.Amount),
+                Date = Data1.Date,
+            });
+
+        return Redirect("/");
+    }
 
     public void OnGet(
         [FromServices] IApplicationService applicationService)
@@ -18,20 +57,48 @@ public class IndexModel : PageModel
         {
             return;
         }
-        
+
         var userId = User.FindFirstValue("sub");
-        
+
         var expenses = applicationService
             .GetExpensesLoggedInUser(ExternalUserId.FromString(userId))
-            .Select(x => new ExpenseDto(x.Name.Value, x.Amount.Value))
+            .Select(x => new ExpenseDto
+            {
+                Expense = x.Name.Value,
+                Amount = x.Amount.Value,
+                Date = x.Date,
+            })
+            .OrderByDescending(x => x.Date)
             .ToList();
-        
-        Data = new(expenses);
+
+        Data = new IndexDto
+        {
+            Expenses = expenses,
+        };
     }
 }
 
-public record IndexDto(List<ExpenseDto> Expenses);
+internal sealed record IndexDto
+{
+    public required List<ExpenseDto> Expenses { get; init; }
+}
 
-public record ExpenseDto(
-    string Expense,
-    decimal Amount);
+internal sealed record ExpenseDto
+{
+    public required string Expense { get; init; }
+
+    [DisplayFormat(DataFormatString = "{0:C}")]
+    public required decimal Amount { get; init; }
+
+    [DisplayFormat(DataFormatString = "{0:g}")]
+    public required DateTime Date { get; init; }
+}
+
+public sealed record Data
+{
+    [Required] public string Expense { get; init; } = null!;
+
+    [Required] public decimal Amount { get; init; }
+    
+    [Required] public DateTime Date { get; init; } = DateTime.Today;
+}
